d5820767e2b52969804d0545283039ea42095327
Roshan Sudam Rathod <roshanrathod22910@gmail.com>
2026-08-05 23:51:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (38.02%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index == -1) {
+            return word;
+        }
+        StringBuilder prefix = new StringBuilder(word.substring(0, index + 1));
+        prefix.reverse();
+        prefix.append(word.substring(index + 1));
+        return prefix.toString();
+    }
+}
